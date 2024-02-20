f99aaf48b11c9a275885b98596d1a2e1c2e63de8
Dhruvi Shah <107160527+Dhruvi224@users.noreply.github.com>
2024-02-20 17:01:23 +0530

    Add files via upload
    =>chcek the number is Even or Odd

---
+++ Ex_second.java
@@ -0,0 +1,14 @@
+package first_program;
+
+public class Ex_second {
+	public static void main(String args[]) {
+		int a=11;
+		if(a%2==0) {
+			System.out.println("it's even");
+		}else {
+			System.out.println("it's odd");
+		}
+		
+	}
+
+}
