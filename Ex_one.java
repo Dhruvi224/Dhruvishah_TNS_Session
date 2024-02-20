f9eb1006b4da643030b0819322acd73b21b7f50d
Dhruvi Shah <107160527+Dhruvi224@users.noreply.github.com>
2024-02-20 16:31:55 +0530

    Add files via upload

---
+++ Ex_one.java
@@ -0,0 +1,14 @@
+package first_program;
+
+public class Ex_one {
+	public static void main(String args[]) {
+		System.out.println("hello");
+		int a=10,b=20;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
