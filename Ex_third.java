8ad0812ded995f8ecd03bb701f0785043ff7321c
Dhruvi Shah <107160527+Dhruvi224@users.noreply.github.com>
2024-02-21 11:36:17 +0530

    Add files via upload

---
+++ Ex_third.java
@@ -0,0 +1,16 @@
+package first_program;
+
+public class Ex_third {
+	public static void main(String args[]) {
+		int a=10,b=50,c=60;
+			if(a>=b && a>=c) {
+				System.out.println(a);
+			}else if(b>=a && b>=c) {
+				System.out.println(b);
+			}else {
+				System.out.println(c);
+			}
+//			var(condition)?true:false
+	}
+
+}
